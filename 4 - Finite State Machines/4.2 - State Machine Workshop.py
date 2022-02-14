e11c8e94f3092ad6e7f938e41218bd74689e9fc5
Michael <84158733+Michael-Schoo@users.noreply.github.com>
2022-02-15 09:11:05 +1100

    Step 1

--- 4 - Finite State Machines/4.2 - State Machine Workshop.py
+++ 4 - Finite State Machines/4.2 - State Machine Workshop.py
@@ -1,5 +1,8 @@
+from typing import Literal
+
 # set initial state 
 # door is closed 
+door: Literal["open" , "closed", "opening", "closing", "stopped"] 
 door = "closed" 
 print("Door is", door) 
 # repeat forever 
@@ -12,6 +15,9 @@ while True:
     # transition to open state 
     elif instruction == "s" and door == "opening": 
         door = "open" 
+    # transition to stopped 
+    elif instruction == "b" and door == "opening": 
+        door = "stopped" 
     # transition to closing state 
     elif instruction == "b" and door == "open": 
         door = "closing" 
